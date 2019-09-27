1812e24711ef2d48d839f82e2a938bbf540e79e5
M <m2bright@yahoo.com>
2019-09-27 15:09:49 -0600

    Adding some organization

--- rapid/client/__init__.py
+++ rapid/client/__init__.py
@@ -90,17 +90,6 @@ def setup_client_register_thread():
 
 def run_action_instance(action_instance_id):
     # type: (int) -> None
-    from rapid.client.executor import Executor
-    from rapid.lib.work_request import WorkRequest
-
-    communicator = ClientCommunicator(app.rapid_config.master_uri, app.rapid_config.quarantine_directory, app, app.rapid_config.verify_certs)  # pylint: dissable=no-member
-    communicator.register(app.rapid_config)
-    request_json = communicator.get_work_request_by_action_instance_id(action_instance_id)
-    executor = Executor(WorkRequest(request_json),
-                        app.rapid_config.master_uri,
-                        workspace=app.rapid_config.workspace,
-                        quarantine=app.rapid_config.quarantine_directory,
-                        verify_certs=app.rapid_config.verify_certs,
-                        rapid_config=app.rapid_config)
-    executor.verify_work_request()
-    executor.start(False)
+    from rapid.client.action_instance_runner import ActionInstanceRunner
+    runner = ActionInstanceRunner(app.rapid_config)
+    runner.run_action_instance(app, action_instance_id)
