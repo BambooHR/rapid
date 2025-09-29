ad4677589632d4850daae6c0a7a694d7e861bd30
Valerie Clough <vclough@bamboohr.com>
2025-09-29 17:25:24 -0600

    GRAPH-3091: Error handling in log parsing. Catch and log errors, and report the failure.

--- rapid/client/parsers/xunit_parser.py
+++ rapid/client/parsers/xunit_parser.py
@@ -14,6 +14,8 @@
  limitations under the License.
 """
 
+import traceback
+import logging
 from xml.dom.minidom import parseString
 from rapid.lib.constants import Constants
 
@@ -32,79 +34,93 @@ class XUnitParser(AbstractParser):
         :return: dict of results.
         """
         results = {}
-        dom = parseString("".join(lines))
         summary = self.prepare_summary()
+        try:
+            dom = parseString("".join(lines))
 
-        for testsuite in dom.getElementsByTagName('testsuite'):
-            for node in testsuite.childNodes:
-                if node.nodeName != 'testcase':
-                    continue
-                testcase = node
-                name = ''
-                try:
-                    class_name = ''
-                    for class_attr in ['classname', 'class']:
-                        class_name = testcase.getAttribute(class_attr)
-                        if class_name:
-                            break
+            for testsuite in dom.getElementsByTagName('testsuite'):
+                for node in testsuite.childNodes:
+                    if node.nodeName != 'testcase':
+                        continue
+                    testcase = node
+                    name = ''
+                    try:
+                        class_name = ''
+                        for class_attr in ['classname', 'class']:
+                            class_name = testcase.getAttribute(class_attr)
+                            if class_name:
+                                break
 
-                    name = "{}~{}".format(class_name, testcase.getAttribute('name'))
-                except AttributeError:
-                    import traceback
-                    traceback.print_exc()
+                        name = "{}~{}".format(class_name, testcase.getAttribute('name'))
+                    except AttributeError:
 
-                if self.workspace:
-                    name = name.replace(self.workspace, '')
+                        traceback.print_exc()
 
-                test_result = {
-                    'status': Constants.STATUS_SUCCESS,
-                    'time': testcase.getAttribute('time')
-                }
+                    if self.workspace:
+                        name = name.replace(self.workspace, '')
 
-                failure_tags = testcase.getElementsByTagName('failure')
-                skipped_tags = testcase.getElementsByTagName('skipped')
-                error_tags = testcase.getElementsByTagName('error')
+                    test_result = {
+                        'status': Constants.STATUS_SUCCESS,
+                        'time': testcase.getAttribute('time')
+                    }
 
-                if self.failures_only and (not failure_tags and not error_tags):
-                    continue
+                    failure_tags = testcase.getElementsByTagName('failure')
+                    skipped_tags = testcase.getElementsByTagName('skipped')
+                    error_tags = testcase.getElementsByTagName('error')
 
-                if failure_tags:
-                    test_result['status'] = Constants.STATUS_FAILED
-                    test_result['stacktrace'] = ""
+                    if self.failures_only and (not failure_tags and not error_tags):
+                        continue
 
-                    count = 0
-                    length = len(failure_tags)
-                    for failure in failure_tags:
-                        test_result['stacktrace'] += "\n".join(
-                            [failure.firstChild.nodeValue if failure.firstChild is not None and hasattr(failure.firstChild, 'nodeValue')
-                             else failure.attributes['message'].value])
-                        count += 1
-                        if length > count:
-                            test_result['stacktrace'] += "\n"
+                    if failure_tags:
+                        test_result['status'] = Constants.STATUS_FAILED
+                        test_result['stacktrace'] = ""
 
-                    summary[Constants.STATUS_FAILED] += 1
-                elif error_tags:
-                    test_result['status'] = Constants.STATUS_FAILED
-                    test_result['stacktrace'] = ""
+                        count = 0
+                        length = len(failure_tags)
+                        for failure in failure_tags:
+                            test_result['stacktrace'] += "\n".join(
+                                [failure.firstChild.nodeValue if failure.firstChild is not None and hasattr(failure.firstChild, 'nodeValue')
+                                 else failure.attributes['message'].value])
+                            count += 1
+                            if length > count:
+                                test_result['stacktrace'] += "\n"
 
-                    count = 0
-                    length = len(failure_tags)
-                    for failure in error_tags:
-                        test_result['stacktrace'] += "\n".join(
-                            ["Error - " + (failure.firstChild.nodeValue if failure.firstChild is not None and hasattr(failure.firstChild, 'nodeValue')
-                                           else failure.attributes['message'].value)])
-                        count += 1
-                        if length > count:
-                            test_result['stacktrace'] += "\n"
+                        summary[Constants.STATUS_FAILED] += 1
+                    elif error_tags:
+                        test_result['status'] = Constants.STATUS_FAILED
+                        test_result['stacktrace'] = ""
 
-                    summary[Constants.STATUS_FAILED] += 1
-                elif skipped_tags:
-                    test_result['status'] = Constants.STATUS_SKIPPED
-                    summary[Constants.STATUS_SKIPPED] += 1
-                else:
-                    summary[Constants.STATUS_SUCCESS] += 1
+                        count = 0
+                        length = len(failure_tags)
+                        for failure in error_tags:
+                            test_result['stacktrace'] += "\n".join(
+                                ["Error - " + (failure.firstChild.nodeValue if failure.firstChild is not None and hasattr(failure.firstChild, 'nodeValue')
+                                               else failure.attributes['message'].value)])
+                            count += 1
+                            if length > count:
+                                test_result['stacktrace'] += "\n"
 
-                results[name] = test_result
+                        summary[Constants.STATUS_FAILED] += 1
+                    elif skipped_tags:
+                        test_result['status'] = Constants.STATUS_SKIPPED
+                        summary[Constants.STATUS_SKIPPED] += 1
+                    else:
+                        summary[Constants.STATUS_SUCCESS] += 1
+
+                    results[name] = test_result
+        except Exception as e:
+            logger = logging.getLogger("rapid")
+            logger.error("Error parsing XML data: %s", e)
+            logger.error(traceback.format_exc())
+
+            results["xml_parse_error"] = {
+                "status": Constants.STATUS_FAILED,
+                "time": 0,
+                "stacktrace": "Error parsing XML data: " + str(e) + "\n" + traceback.format_exc()
+            }
+            summary[Constants.STATUS_FAILED] += 1
+            results["__summary__"] = summary
+            return results
 
         results['__summary__'] = summary
 
